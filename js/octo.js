972e5be30ae80d6e2f734c335c72bc416956c434
Stanislav Mikhaylov <ceo@adflare.ru>
2016-01-13 11:03:48 +0300

    Init

---
+++ js/octo.js
@@ -0,0 +1,6 @@
+/**
+ * Created by stas on 11.01.2016.
+ */
+window.octo = class octo {
+
+}
---
+++ js/q-inj.js
@@ -0,0 +1,17 @@
+(function(g,r,e,a,t){
+    var a = g.createElement(r);
+    var t = g.getElementsByTagName(r)[0];
+    a.async = 1;
+    a.src = e;
+    t.parentNode.insertBefore(a, t)
+})(document, 'script', 'js/q.js');
+window.q = new q({
+    'debugMode': 1,
+    'appId': 'alpha'
+});
+window.q.err = new err;
+window.q.events = new events;
+
+//short
+
+(function(g,r,e,a,t){ var a = g.createElement(r); var t = g.getElementsByTagName(r)[0]; a.async = 1; a.src = e; t.parentNode.insertBefore(a, t) })(document, 'script', 'js/q.js'); window.q = new q({ 'debugMode': 1, 'appId': 'alpha' }); window.q.err = new err; window.q.events = new events;
